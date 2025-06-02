21b8b1222ce6ccf2fb1f0881a37ecd63a9930b0d
Sairam Chidurala <sairam.chidurala@nextiva.com>
2025-06-03 03:19:42 +0530

    template fixed

--- fileuploader/settings.py
+++ fileuploader/settings.py
@@ -45,6 +45,22 @@ DATABASES = {
     )
 }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # Static files - only fix the typo
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
