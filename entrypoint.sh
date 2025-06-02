df11f51920cfa937064719508e2dd4720b198252
Sairam Chidurala <sairam.chidurala@nextiva.com>
2025-06-03 01:35:07 +0530

    env added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
+# #!/bin/sh
+
+# python manage.py migrate --noinput
+# gunicorn fileuploader.wsgi:application --bind 0.0.0.0:8000 --workers 4
+
 #!/bin/sh
 
+# Export variables from .env file if it exists
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 gunicorn fileuploader.wsgi:application --bind 0.0.0.0:8000 --workers 4
